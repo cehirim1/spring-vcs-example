3ab811dc3ab674e3ddeb69817ffc45ef3da65bf9
cehirim1 <Humbled1st@gmail.com>
2023-01-09 13:41:23 -0800

    Committed entry point files and pom.xml file

---
+++ src/main/java/edu/greenriver/sdev/project_under_vcs/ProjectUnderVcsApplication.java
@@ -0,0 +1,13 @@
+package edu.greenriver.sdev.project_under_vcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectUnderVcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectUnderVcsApplication.class, args);
+    }
+
+}
